978955e69e2fea0c5081259624e7ceb310e47339
GravyDown <145372183+GravyDown@users.noreply.github.com>
2023-09-23 12:08:52 +0530

    Create sum_of_series_like_(2+22+222+2222...).py

---
+++ sum_of_series_like_(2+22+222+2222...).py
@@ -0,0 +1,12 @@
+#13_program to calculate the sum of series upto n terms
+n=int(input("Enter value: "))
+l1=[2]
+s=2
+for i in range(1,n):
+  if n==1:
+    break
+  else:
+    x=2*(10**i)
+    s+=x
+    l1.append(s)
+print("Sum upto",n,"terms is:",sum(l1))
